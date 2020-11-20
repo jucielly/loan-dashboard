c7f154f997986931a05a8dcfc0d0ba7bfb0672c2
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-11-20 13:19:41 -0300

    created value card

---
+++ src/components/molecules/ValueCard/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import SimpleCard from '../../atoms/SimpleCard';
+
+const StyledValueCard = styled(SimpleCard)`
+  padding: 30px;
+  display: flex;
+  color: ${(props) => props.theme.colors.fontColor};
+  height: 200px;
+  width: 400px;
+  justify-content: space-between;
+  align-items: center;
+
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    span {
+      color: ${(props) => props.theme.colors.primary};
+      font-size: ${(props) => props.theme.fonts.fontSizes.medium};
+    }
+  }
+
+  .icon {
+    color: ${(props) => props.theme.colors.helper};
+    font-size: 80px;
+  }
+`;
+
+const ValueCard = ({ title, icon, value }) => {
+  return (
+    <StyledValueCard>
+      <div className="container">
+        <h1>{title}</h1>
+        <span>{value}</span>
+      </div>
+      <div className="icon">{icon}</div>
+    </StyledValueCard>
+  );
+};
+
+ValueCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.element,
+  value: PropTypes.number.isRequired,
+};
+
+export default ValueCard;
