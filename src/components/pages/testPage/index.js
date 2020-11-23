66a8a569faef4a998e8ffb4e33579e0e775bd118
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-11-23 15:38:38 -0300

    Header Finished

--- src/components/pages/testPage/index.js
+++ src/components/pages/testPage/index.js
@@ -1,48 +1,8 @@
 import React from 'react';
-import SimpleCard from '../../atoms/SimpleCard';
-import Button from '../../atoms/Button';
-import Input from '../../atoms/input';
-import styled from 'styled-components';
-import Avatar from '../../atoms/Avatar';
-import ValueCard from '../../molecules/ValueCard';
-import { GiReceiveMoney } from 'react-icons/gi';
-import InstallmentListItem from '../../molecules/InstallmentLIstItem';
-import LoanSimulatorForm from '../../molecules/LoanSimulatorForm';
+import Header from '../../organisms/Header';
 
 const Test = () => {
-  return (
-    <>
-      <CardTeste>
-        <h1>Atom Test Card</h1>
-        <Input placeholder="Taxa de Juros" />
-        <Input placeholder="Taxa de Juros" />
-        <Button>Quero esse</Button>
-      </CardTeste>
-      <Avatar />
-      <ValueCard
-        title="Total Emprestimo"
-        icon={<GiReceiveMoney />}
-        value={`R$ ${2500}`}
-      />
-      <InstallmentListItem value={`R$ ${500}`} date="17/10/20" />
-      <InstallmentListItem value={`R$ ${500}`} date="17/10/20" />
-      <InstallmentListItem value={`R$ ${500}`} date="17/10/20" />
-      <LoanSimulatorForm />
-    </>
-  );
+  return <Header />;
 };
 
 export default Test;
-
-const CardTeste = styled(SimpleCard)`
-  h1 {
-    margin-bottom: 100px;
-    color: ${(props) => props.theme.colors.fontColor};
-  }
-
-  width: 400px;
-  height: 400px;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
